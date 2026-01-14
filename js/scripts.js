69ab44be3097907221f6d4e05d9229b8472f3947
Armel Tonleu <armeltonleu@gmail.com>
2026-01-14 15:17:43 +0100

    starting writing the script

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 const form = document.getElementById("form");
+const errorMsg = document.getElementById("error-msg");
+
+function formSubmit(event) {
+  event.preventDefault();
+  console.log("formulaire soumis");
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const email = document.getElementById("email");
+
+  if (!regex.test(email)) {
+    console.log("mauvais email");
+    form.classList.toggle("error");
+  }
+}
+
+function logSubmit(event) {
+  event.preventDefault();
+  console.log("coucou");
+}
+
+form.addEventListener("submit", logSubmit);
