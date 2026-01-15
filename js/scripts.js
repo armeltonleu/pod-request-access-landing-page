beb13e1cb203b9b372438b417aff11a859260e8d
Armel Tonleu <armeltonleu@gmail.com>
2026-01-15 15:10:21 +0100

    script ok

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,21 @@ const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 function formError(msg) {
   form.classList.remove("success");
   form.classList.add("error");
-  errorMsg.style.display = "block";
+  errorMsg.style.height = "100%";
+  errorMsg.style.opacity = "1";
   errorMsg.textContent = msg;
 }
 function formSuccess() {
   form.classList.remove("error");
   form.classList.add("success");
-  errorMsg.style.display = "none";
+  errorMsg.style.height = "0";
+  errorMsg.style.opacity = "0";
 }
 
 form.addEventListener("submit", (e) => {
   const emailValue = email.value.trim();
 
-  if (email === "") {
+  if (emailValue === "") {
     e.preventDefault();
     console.log(emailValue);
     formError("Oops! Please add your email");
