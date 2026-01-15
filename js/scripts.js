883a84bb18de29462fcdf3e59e0a084c86ef3923
Armel Tonleu <armeltonleu@gmail.com>
2026-01-15 08:06:33 +0100

    empty field checked

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,34 @@
 const form = document.getElementById("form");
+const email = document.getElementById("email");
 const errorMsg = document.getElementById("error-msg");
+const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-function formSubmit(event) {
-  event.preventDefault();
-  console.log("formulaire soumis");
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const email = document.getElementById("email");
+function formError(msg) {
+  form.classList.remove("success");
+  form.classList.add("error");
+  errorMsg.style.display = "block";
+  errorMsg.textContent = msg;
+}
+function formSuccess() {
+  form.classList.remove("error");
+  form.classList.add("success");
+  errorMsg.style.display = "none";
+}
 
-  if (!regex.test(email)) {
-    console.log("mauvais email");
-    form.classList.toggle("error");
+form.addEventListener("submit", (e) => {
+  const emailValue = email.value.trim();
+
+  if (email === "") {
+    e.preventDefault();
+    console.log(emailValue);
+    formError("Oops! Please add your email");
+  } else if (!regex.test(emailValue)) {
+    e.preventDefault();
+    console.log(emailValue);
+    formError("Oops! Please check your email");
+  } else {
+    e.preventDefault();
+    console.log(emailValue);
+    formSuccess();
   }
-}
-
-function logSubmit(event) {
-  event.preventDefault();
-  console.log("coucou");
-}
-
-form.addEventListener("submit", logSubmit);
+});
